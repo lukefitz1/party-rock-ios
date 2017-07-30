53e9d3bc5ca9f18e11eaacdce7aa3be5e847707a
Luke Fitzgerald <luke.fitzgerald@blueacorn.com>
2017-07-30 19:38:37 -0400

    adding party rock ios app to github

---
+++ PartyRockApp/PartyCell.swift
@@ -0,0 +1,41 @@
+//
+//  PartyCell.swift
+//  PartyRockApp
+//
+//  Created by Luke Fitzgerald on 7/10/17.
+//  Copyright © 2017 Luke Fitzgerald. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    @IBOutlet weak var videoTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func updateUI(partyRock: PartyRock) {
+        videoTitle.text = partyRock.videoTitle
+        
+        let url = URL(string: partyRock.imageURL)!
+        
+        //puts operations on a background thread
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+                
+            } catch {
+                //handle the error
+            }
+        }
+    }
+
+
+}
